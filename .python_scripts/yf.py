1736772a270350ca57b43ea7faa40c79a4fa91c9
Varun Iyer <viyer9@uic.edu>
2026-04-14 07:08:38 +0000

    update files

--- .python_scripts/yf.py
+++ .python_scripts/yf.py
@@ -3,20 +3,25 @@ import sys
 from pathlib import Path
 
 
-def concat_files(files: list[Path]) -> bytes:
-    return "\n\n".join(
-        f"=== File: {path.resolve().as_posix()} ===\n{path.read_text()}"
-        for path in files
-    ).encode()
+def concat_files(files: list[Path]) -> tuple[int, bytes]:
+    contents: list[str] = []
+    for path in files:
+        try:
+            contents.append(
+                f"=== File: {path.resolve().as_posix()} ===\n{path.read_text()}"
+            )
+        except UnicodeDecodeError:
+            pass
+    return len(contents), "\n\n".join(contents).encode()
 
 
 def process_files(args: list[str]) -> int:
     dir, files = args[1], args[2:]
 
     dir_path = Path(dir)
-    data = concat_files([dir_path / f for f in files])
+    num_files, data = concat_files([dir_path / f for f in files])
     rc = subprocess.run(["fish", "-ic", "yi"], input=data).returncode
-    print(f"Done! Processed {len(files)} files.")
+    print(f"Done! Processed {num_files}/{len(files)} files.")
     return rc
 
 
